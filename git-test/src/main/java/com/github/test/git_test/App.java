75a3819a2bd03f9310745657be370d6c739b34eb
raviprakash.m <raviprakash.m@LP-5CD7490BVR.HCLT.CORP.HCL.IN>
2019-07-17 12:14:05 +0530

    committing maven project

---
+++ git-test/src/main/java/com/github/test/git_test/App.java
@@ -0,0 +1,15 @@
+package com.github.test.git_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("push and commit");
+    }
+}
